fa433cd3f26b517d1bc713395dd611c1f9bc30d6
Nathan <9020870@student.tcrmbo.nl>
2025-05-16 09:13:43 +0200

    some html to start of this side of the website and added image folder.

--- src/view/gameview.php
+++ src/view/gameview.php
@@ -12,6 +12,25 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <h1>Games</h1>
+    <nav>
+        <ul>
+            <li><a href="?page=home">Home</a></li>
+            <li><a href="?page=products">Products</a></li>
+            <li><a href="?page=games">Games</a></li>
+            <li><a href="?page=about">About Us</a></li>
+            <li><a href="?page=contact">Contact</a></li>
+        </ul>
+    </nav>
+
+    <h2>Available Games</h2>
+    <ul>
+        <li>Game 1 - $19.99</li>
+        <li>Game 2 - $29.99</li>
+        <li>Game 3 - $39.99</li>
+        <li>Game 4 - $49.99</li>
+        <li>Game 5 - $59.99</li>
+        <li>Game 6 - $69.99</li>
+    </ul>
 </body>
 </html>
